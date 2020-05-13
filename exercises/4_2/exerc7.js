750320ef427b8d05c71f8096cd42ebaadaa3cf12
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-13 18:26:41 -0300

    Exercicio 7

---
+++ exercises/4_2/exerc7.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = numbers[0];
+
+
+for (let i=0; i<numbers.length; i++) {
+	if (numbers[i] < menor){
+		menor = numbers[i];
+	}
+}
+
+console.log("O menor valor contido no array é: " + menor);
