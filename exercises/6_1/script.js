c0f427fcbf9733cbfce5102eed91ef5f2c0186f3
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-01 19:17:59 -0300

    tudo resolvido

---
+++ exercises/6_1/script.js
@@ -0,0 +1,37 @@
+let stateList = [
+  ['Acre', 'AC'],
+  ['Alagoas', 'AL'],
+  ['Amapá', 'AP'],
+  ['Amazonas', 'AM'],
+  ['Bahia', 'BA'],
+  ['Ceará', 'CE'],
+  ['Distrito Federal', 'DF'],
+  ['Espírito Santo', 'ES'],
+  ['Goiás', 'GO'],
+  ['Maranhão', 'MA'],
+  ['Mato Grosso', 'MT'],
+  ['Mato Grosso do Sul', 'MS'],
+  ['Minas Gerais', 'MG'],
+  ['Pará', 'PA'],
+  ['Paraíba', 'PB'],
+  ['Paraná', 'PR'],
+  ['Pernambuco', 'PE'],
+  ['Piauí', 'PI'],
+  ['Rio de Janeiro', 'RJ'],
+  ['Rio Grande do Norte', 'RN'],
+  ['Rio Grande do Sul', 'RS'],
+  ['Rondônia', 'RO'],
+  ['Roraima', 'RR'],
+  ['Santa Catarina', 'SC'],
+  ['São Paulo', 'SP'],
+  ['Sergipe', 'SE'],
+  ['Tocantins', 'TO']
+];
+
+let pegaPosDropDown = document.getElementById('estado');
+for (let i=0; i<stateList.length; i += 1) {
+  let criaOption = document.createElement('option');
+  criaOption.innerHTML = stateList[i][0] + ' - ' + stateList[i][1];
+  criaOption.value = stateList[i][1];
+  pegaPosDropDown.append(criaOption);
+}
