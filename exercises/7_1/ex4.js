da627c8c6bafc2326479ca20d56fa08e0797273d
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-14 15:29:06 -0300

    todos os exercicios do dia

--- exercises/7_1/ex4.js
+++ exercises/7_1/ex4.js
@@ -8,11 +8,39 @@ Exemplo:
 String determinada: “Tryber x aqui!”
 Parâmetro: “Bebeto”
 Retorno: “Tryber Bebeto aqui!”
-Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
-Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string.
+ */
+const procuraX = (string, parametro) => {
+  let vetorAux = string.split(' ');
+  for (let i=0; i < vetorAux.length; i += 1) {
+    for (let j=0; j < vetorAux[i].length; j += 1) {
+      if (vetorAux[i][j] === 'x' || vetorAux[i][j] == 'X') {
+        vetorAux[i] = parametro;
+      }
+    }
+  }
+  return vetorAux.join(' ');
+}
+console.log(procuraX('Tryber x faz coisas que ninguém imagina', 'rafael'));
+
+/* Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro.
+Essa função deve concatenar as skills do array global à string que foi passada para a Função 2
+via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar
+essa nova string.
 Exemplo:
 “Tryber x aqui!
 Minhas cinco principais habilidades são:
 JavaScript;
 HTML; …
 #goTrybe”. */
+
+const variavelGlobal = `Minhas cinco principais habilidades são:
+Javascript;
+HTML;
+CSS;
+Python;
+Linux;
+#goTrybe`;
+
+const concatenaSkills = string => `${string} e agora ${variavelGlobal}`;
+
+console.log(concatenaSkills(procuraX('Tryber x faz coisas que ninguém imagina', 'rafael')));
