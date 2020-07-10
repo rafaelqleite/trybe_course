42c2cc754efc390f8fd393c7038133e1da7e4998
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-10 12:42:51 -0300

    estilizado

--- exercises/11_2/my-pokedex/src/Pokemon.js
+++ exercises/11_2/my-pokedex/src/Pokemon.js
@@ -4,14 +4,16 @@ class Pokemon extends React.Component {
   render(){
     const { name, image, type, averageWeight: { value, measurementUnit } } = this.props.abestado;
     return (
-      <p>
       <div className='wrapping-pokemonUnit'>
-        <b>{ name }</b><br/>
-        <img src={ image } alt = {name}/><br/>
-        Tipo: <strong><em>{ type }</em></strong><br/>
-        Peso: <em><b>{value} {measurementUnit}</b></em><br/>
+        <div className='left'>
+          Nome: <b>{ name }</b><br/>
+          Tipo: <strong><em>{ type }</em></strong><br/>
+          Peso: <em><b>{value} {measurementUnit}</b></em><br/>
+        </div>
+        <div className='right'>
+          <img src={ image } alt = {name}/><br/>
+        </div>
       </div>
-      </p>
     );
   }
 }
