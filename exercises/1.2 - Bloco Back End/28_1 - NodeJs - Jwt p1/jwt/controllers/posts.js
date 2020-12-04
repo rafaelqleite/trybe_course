f39d387c6a084c6eff6a89e955d741676a3c4778
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-12-04 20:10:05 -0300

    submit

---
+++ exercises/1.2 - Bloco Back End/28_1 - NodeJs - Jwt p1/jwt/controllers/posts.js
@@ -0,0 +1,23 @@
+const mockPosts = [
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+];
+
+module.exports = (req, res, next) => {
+  console.log('no posts do controllers - req.user', req.user);
+  res.status(200).json({ mockPosts });
+};
